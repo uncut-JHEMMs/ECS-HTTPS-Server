3e23ef22f3820b5cbebc7f4f6909cb0496ed6761
SkinlessMilk3 <ethancoreyseiber@hotmail.com>
2022-02-03 13:36:24 -0600

    JHEMMS-137 Finished setting up and testing local root CA

---
+++ create_CA.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$1" == "test" ] && [ "$1" == "Test" ]
+then
+#test if certificate was generated     
+    openssl x509 -noout -text -in root/ca/certs/ca.cert.pem
+
+elif [ $1 == clean ]
+then
+    rm -r root/**
+    rmdir root
+else
+    mkdir root
+    mkdir root/ca
+    mkdir root/ca/private
+    mkdir root/ca/certs
+    mkdir root/ca/newcerts
+    mkdir root/ca/crl
+    touch root/ca/index.txt
+    mv openssl.cnf root/ca/
+    
+    chmod 700 root/ca/private
+    
+    openssl genrsa -aes256 -out root/ca/private/ca.key.pem 4096
+    chmod 400 root/ca/private/ca.key.pem
+    
+    openssl req -new -x509 -nodes -days 365000  -key root/ca/private/ca.key.pem -out root/ca/certs/ca.cert.pem
+    chmod 444 root/ca/certs/ca.cert.pem
+fi
