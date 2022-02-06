39b7b1120034e0b2d8a6d3c17237011d72f0223c
SkinlessMilk3 <ethancoreyseiber@hotmail.com>
2022-02-05 20:33:58 -0600

    JHEMMS-138: Got server csr signing implemented.

--- create_CA.sh
+++ create_CA.sh
@@ -1,31 +1,67 @@
 #!/bin/bash
 
+clear
+
 if [ "$1" == "test" ] || [ "$1" == "Test" ]
 then
-#test if certificate was generated     
-    openssl x509 -noout -text -in root/ca/certs/ca.cert.pem
-
+    #test if certificate was generated
+    echo "Verify root CA certificate"
+    openssl x509 -noout -text -in root/ca/certs/root.ca.cert.pem
+    echo "Verify server csr was signed by root CA"
+    openssl verify -CAfile root/ca/certs/root.ca.cert.pem server/ca/certs/server.ca.cert.pem
+    
 elif [ $1 == clean ]
 then
-    mv root/ca/openssl.cnf .
     rm -r root/**
     rmdir root
+    rm -r server/**
+    rmdir server
     
+#Generate local root CA. Then sign a server certificate.
 else
     mkdir root
     mkdir root/ca
-    mkdir root/ca/private
-    mkdir root/ca/certs
-    mkdir root/ca/newcerts
-    mkdir root/ca/crl
+
+    cd root/ca/
+    
+    mkdir private certs newcerts crl
     touch root/ca/index.txt
-    mv openssl.cnf root/ca/
+    touch root/ca/serial
     
     chmod 700 root/ca/private
+    touch index.txt
+    echo '01' > serial
     
-    openssl genrsa -aes256 -out root/ca/private/ca.key.pem 4096
-    chmod 400 root/ca/private/ca.key.pem
+    #Generate a private key
+    openssl genrsa -aes256 -out private/root.ca.key.pem 4096
+    chmod 400 private/root.ca.key.pem
+
+    #Generate the CA
+    openssl req -new -x509 -config ../../openssl-ca.cnf -days 365000 -key private/root.ca.key.pem -out certs/root.ca.cert.pem
+    chmod 444 certs/root.ca.cert.pem
+
+    cd ../../
+    mkdir server
+    mkdir server/ca
+    cd server/ca/
+
+    mkdir private certs
+    touch index.txt
+    echo "01" > serial
     
-    openssl req -new -x509 -config root/ca/openssl.cnf -nodes -days 365000  -key root/ca/private/ca.key.pem -out root/ca/certs/ca.cert.pem
-    chmod 444 root/ca/certs/ca.cert.pem
+    chmod 700 private
+
+    #Generate server private key
+    openssl genrsa -aes256 -out private/server.ca.key.pem 4096
+    chmod 400 private/server.ca.key.pem
+
+    openssl req -new -key private/server.ca.key.pem -out server.csr
+    
+    echo
+    echo
+    echo "Generate certificate from csr"
+    cd ../../
+
+    #Generate a certificate for the server from .csr
+    openssl ca -config openssl-ca.cnf -out server/ca/certs/server.ca.cert.pem -infiles server/ca/server.csr
 fi
