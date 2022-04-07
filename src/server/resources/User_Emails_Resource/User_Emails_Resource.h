c30f91c57405832d3a26a2a11b4f5f7a8af6fe92
ss-ethcseib <ethan.seiber@smoothstack.com>
2022-04-06 21:40:28 -0500

    JHEMMS-339: Moved User_Email resource

--- src/server/resources/User_Emails_Resource/User_Emails_Resource.h
+++ src/server/resources/User_Emails_Resource/User_Emails_Resource.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include "../../charger_utils/DocumentSigning.h"
 #include <memory>
+#include "../Digest_Resource.h"
 
 class Email_Gen_resource : public httpserver::http_resource {
 
@@ -11,6 +12,9 @@ private:
   
   const std::shared_ptr<httpserver::http_response> render_GET(const httpserver::http_request& req){
 
+    Digest_Resource digest;
+    digest.render_GET(req);
+    
     DocumentSigning signer;
     signer.sign("server/resources/User_Emails_Resource/UserData.xml");
     
